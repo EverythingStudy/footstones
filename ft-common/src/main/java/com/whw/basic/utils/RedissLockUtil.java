8afc14bcaa33449472eb15d77ac7c23d9f0f5db3
chenlinya <chenlinya@songguo7.com>
2021-09-03 16:46:40 +0800

    all

---
+++ ft-common/src/main/java/com/whw/basic/utils/RedissLockUtil.java
@@ -0,0 +1,170 @@
+package com.whw.basic.utils;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@Component
+public class RedissLockUtil {
+    @Autowired
+    RedissonClient redissonClient;
+    @Value("${redis.lock.key:ebike}")
+    private String lockKeyPerfix;
+
+    @Value("${redis.lock.enable:true}")
+    private boolean lockEnable;
+
+
+    /**
+     * 加锁
+     *
+     * @param lockKey
+     * @return
+     */
+    public RLock lock(String lockKey) {
+        RLock lock = redissonClient.getLock(lockKey);
+        lock.lock();
+        return lock;
+    }
+
+    /**
+     * 释放锁
+     *
+     * @param lockKey
+     */
+    public void unlock(String lockKey) {
+        RLock lock = redissonClient.getLock(lockKey);
+        lock.unlock();
+    }
+
+    /**
+     * 释放锁 忽略异常
+     *
+     * @param lockKey
+     */
+    public void unlockException(String lockKey) {
+        try {
+            if (lockEnable){
+                return;
+            }
+            RLock lock = redissonClient.getLock(lockKey);
+            lock.unlock();
+        } catch (Exception e) {
+            log.error("释放锁异常：" + lockKey, e);
+        }
+    }
+
+    /**
+     * 释放锁
+     *
+     * @param lock
+     */
+    public static void unlock(RLock lock) {
+        lock.unlock();
+    }
+
+    /**
+     * 带超时的锁
+     *
+     * @param lockKey
+     * @param timeout 超时时间   单位：秒
+     */
+    public RLock lock(String lockKey, int timeout) {
+        RLock lock = redissonClient.getLock(lockKey);
+        lock.lock(timeout, TimeUnit.SECONDS);
+        return lock;
+    }
+
+    /**
+     * 带超时的锁
+     *
+     * @param lockKey
+     * @param unit    时间单位
+     * @param timeout 超时时间
+     */
+    public RLock lock(String lockKey, TimeUnit unit, int timeout) {
+        RLock lock = redissonClient.getLock(lockKey);
+        lock.lock(timeout, unit);
+        return lock;
+    }
+
+    /**
+     * 尝试获取锁
+     *
+     * @param lockKey
+     * @param waitTime  最多等待时间
+     * @param leaseTime 上锁后自动释放锁时间
+     * @return
+     */
+    public boolean tryLock(String lockKey, int waitTime, int leaseTime) {
+        if (lockKey == null) {
+            return false;
+        }
+        RLock lock = redissonClient.getLock(lockKey);
+        try {
+            return lock.tryLock(waitTime, leaseTime, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            return false;
+        }
+    }
+
+    /**
+     * 尝试获取锁 出现异常返回指定的数据
+     *
+     * @param lockKey
+     * @param waitTime  最多等待时间
+     * @param leaseTime 上锁后自动释放锁时间
+     * @return
+     */
+    public boolean tryLock(String lockKey, int waitTime, int leaseTime, boolean flag) {
+        if (lockEnable){
+            return true;
+        }
+        if (lockKey == null) {
+            return false;
+        }
+        try {
+            RLock lock = redissonClient.getLock(lockKey);
+            return lock.tryLock(waitTime, leaseTime, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            log.error("获取锁异常：" + lockKey, e);
+            return flag;
+        }
+    }
+
+    /**
+     * 尝试获取锁
+     *
+     * @param lockKey
+     * @param unit      时间单位
+     * @param waitTime  最多等待时间
+     * @param leaseTime 上锁后自动释放锁时间
+     * @return
+     */
+    public boolean tryLock(String lockKey, TimeUnit unit, int waitTime, int leaseTime) {
+        if (lockKey == null) {
+            return false;
+        }
+        RLock lock = redissonClient.getLock(lockKey);
+        try {
+            return lock.tryLock(waitTime, leaseTime, unit);
+        } catch (InterruptedException e) {
+            return false;
+        }
+    }
+
+    public String getLockKey(String bikeSn) {
+        if (bikeSn == null) {
+            return null;
+        }
+        return lockKeyPerfix + ":" + bikeSn;
+    }
+
+}
